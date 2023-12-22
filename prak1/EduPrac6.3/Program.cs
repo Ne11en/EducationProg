502bc6be0bfdf5d8c15dc85dae861fb0723db107
Ne11en <154066588+Ne11en@users.noreply.github.com>
2023-12-22 21:29:54 +0700

    Delete prak1 directory

--- prak1/EduPrac6.3/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Введите число: ");
-
-int num = Int32.Parse(Console.ReadLine());
-
-if (num % 10 == 0)
-{
-    Console.WriteLine("Введенное число является четным и кратным 10");
-}
